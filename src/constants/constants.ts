0108f7212ad8f8577b7a8455f4e8c2a3fa6b9552
kle0247 <karolyn.le23@gmail.com>
2023-02-17 17:28:35 -0500

    installed tailwind

---
+++ src/constants/constants.ts
@@ -0,0 +1,38 @@
+export const projects = [ 
+    {
+        "title": "Vibe Check", 
+        "description": "Connect to Spotify and visualize the features of your top and recent tracks",
+        "techUsed":"Node.js, Express, PostgreSQL, React/Redux, Nivo.js, HTML, CSS",
+        "image": "image1",
+        "source": "https://github.com/kle0247/Vibe-Check",
+        "demo": "https://www.youtube.com/watch?v=gKZEi50hhxo&ab_channel=KarolynLe", 
+        "id": 1
+    },
+    {
+        "title": "Upwards", 
+        "description": "description ",
+        "techUsed":"tech used",
+        "image": "image2",
+        "source": "link",
+        "demo": null,
+        "id": 2
+    },
+    {
+        "title": "Something Sweet", 
+        "description": "description ",
+        "techUsed":"tech used",
+        "image": "image2",
+        "source": "link",
+        "demo": null,
+        "id": 3
+    }, 
+    {
+        "title": "Simple Focus", 
+        "description": "description ",
+        "techUsed":"tech used",
+        "image": "image2",
+        "source": "link",
+        "demo": null,
+        "id": 4
+    }
+];
